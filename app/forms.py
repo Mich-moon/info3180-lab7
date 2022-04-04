def1dc267ec3e1ca0a3c91adf95a0045c463264d
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2022-04-04 17:53:55 -0500

    edited forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,12 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import TextAreaField
+from wtforms.validators import DataRequired
+
+
+class UploadForm(FlaskForm):
+    description = TextAreaField('Message', validators=[DataRequired()])
+    photo = FileField('Image upload', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
+    ])
